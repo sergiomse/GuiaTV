6675464c0cf2bfee3e02599dda32379211cec2f1
smillase <sergiomse@gmail.com>
2015-07-08 20:24:58 +0200

    Poca cosa

--- app/src/main/java/cartelera/turnodetarde/example/com/MainActivity.java
+++ app/src/main/java/cartelera/turnodetarde/example/com/MainActivity.java
@@ -26,12 +26,19 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -113,8 +120,18 @@ public class MainActivity extends Activity {
                 }
 
                 private void loadPrograms(Node node, Channel channel) {
+                    StringWriter sw = new StringWriter();
+                    try {
+                        Transformer t = TransformerFactory.newInstance().newTransformer();
+                        t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                        t.setOutputProperty(OutputKeys.INDENT, "yes");
+                        t.transform(new DOMSource(node), new StreamResult(sw));
+                    } catch (TransformerException te) {
+                        System.out.println("nodeToString Transformer Exception");
+                    }
+                    Log.d(TAG, sw.toString());
                     XPath xpath = XPathFactory.newInstance().newXPath();
-                    String expression = "/div/a";
+                    String expression = "./div/a";
 
                     try {
                         Node textNode = (Node) xpath.evaluate(expression, node, XPathConstants.NODE);
